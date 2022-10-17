fd720d0f3ac7fa00cb825c8ff5ad8f7a97979efd
tttd16 <tiendung.work16@gmail.com>
2022-10-18 02:45:09 +0700

    homework1

--- homework1/main.js
+++ homework1/main.js
@@ -1,10 +1,30 @@
 // Câu 1
+let arr = [4, 5,6,8,4,7,8,9]
+let coutNum = (arr, a) => {
+    let count = 0;
+    for(let i=0; i< arr.length; i++) {
+        if(arr[i] == a) {
+            count++;
+        }
+    }
+    console.log('số'+' '+a+' '+'xuất hiện trong mảng số lần'+' '+count)
+}
+coutNum(arr, 4)
 
+let diffNumArr= arr.reduce(function(arrNew, number) {
+    if (arrNew.indexOf(number) === -1) {
+        arrNew.push(number)
+      }
+      return arrNew
+}, [])
+console.log(diffNumArr)
+let amountNum = diffNumArr.length
+console.log('số phần tử khác nhau trong mảng'+' ' +amountNum)
 
 
 // Câu 2
 // cách 1
-// let n = (a) => {
+// let maxNum = (a) => {
 //     if(a<= 0) {
 //         console.log('sai')
 //     } else {
@@ -18,7 +38,7 @@
 //         } while (a /= 10)
 //     }
 // }
-// console.log(n(1258))
+// console.log(maxNum(1258))
 
 //cách 2
 // function maxNum(n) {
@@ -26,4 +46,5 @@
 //      return arr[0]
 // }
 // console.log(maxNum(26349))
+
 
